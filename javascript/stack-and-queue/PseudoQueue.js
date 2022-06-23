4fd00c88a24e6d6d68a5d7a619ccb59f2bee0ffc
hadeel999 <hadeelahmadsaleh@gmail.com>
2022-06-23 13:15:06 +0300

    Ch11

---
+++ javascript/stack-and-queue/PseudoQueue.js
@@ -0,0 +1,24 @@
+'use strict';
+const Stack = require('./Stack');
+
+class PseudoQueue {
+    constructor() {
+        this.stack1 = new Stack();
+        this.stack2 = new Stack();
+    }
+
+    enqueue(value) {
+        this.stack1.push(value);
+    }
+
+    dequeue() {
+       if (this.stack1.top){
+        this.stack2.push(this.stack1.top.value);
+        this.stack1.pop();
+       }else{
+        return "Exception";
+       }
+    }
+    
+}
+module.exports = PseudoQueue;
